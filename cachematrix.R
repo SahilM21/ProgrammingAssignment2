c36899527cc2b3c4ff23dcd23b924c00db8bddbd
SahilM21 <53071032+SahilM21@users.noreply.github.com>
2020-02-15 21:38:27 -0800

    Add files via upload
    Coursera Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,49 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # cached inverse of matrix
+  invrs <- NULL
+  
+  ## get/set for matrix
+  get <- function() x
+  set <- function(y) {
+    x <<- y
+    invrs <<- NULL
+  }
+  
+  ## get/set for matrix inverse
+  getinvrs <- function() invrs
+  setinvrs <- function(inverse) invrs <<- inverse
+  
+  ## return list of functions for matrix
+  list(get=get, set=set, getinvrs=getinvrs, setinvrs=setinvrs)
 }
 
-
-## Write a short comment describing this function
-
+# Computes the inverse of a matrix. If the inverse has already been
+# calculated before, the cached inverse is returned.
+#
+# Args:
+#   x: A matrix
+#   ...: Extra arguments
+#
+# Returns:
+#   The inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invrs <- x$getinvrs()
+  
+  # return cached matrix inverse if it's been already computed
+  if (!is.null(invrs)) {
+    message("inverse is cached")
+    return(invrs)
+  }
+  
+  # compute inverse of matrix 
+  m <- x$get()
+  invrs <- solve(m, ...)
+  
+  # cache inverse
+  x$setinvrs(invrs)
+  
+  # return inverse of matrix
+  return(invrs)
 }
